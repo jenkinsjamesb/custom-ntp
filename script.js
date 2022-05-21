7f1dad6c0dc573043f688f32661f0bf8938ff33a
Brad J <jenkins.j.brad@gmail.com>
2022-05-21 15:20:41 +0400

    Fixes & weather structure for future BRad to deal with.

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // listener for dynamic search bar
     var elem = document.querySelector("input[type='text']");
     elem.oninput = () => {
         elem.style.width = (elem.value.length + 1) + 'ch';
     }
+
+    // import background
+    weatherEnabled = false;
+    if (weatherEnabled) {
+        var script = document.createElement("script");
+        script.src = "resources/weather.js"
+        document.head.appendChild(script)
+    };
 });
-
-// TODO: append to background?
-/*//weather
-navigator.geolocation.getCurrentPosition((pos) => {
-    //console.log(pos.coords.accuracy);
-})
-// feed to https://forecast.weather.gov/MapClick.php?lat=38.301180000000045&lon=-77.45932999999997*/
-
-/* 
-fetch("resources/tabs/" + name + ".html").then((response) => {
-    return response.text().then((text) => {
-        document.getElementById("content").innerHTML = text;
-    });
-});
-*/
-
-//modify precip path by wind speed, pick rand left/right
-//sun rays, dust, wind, rain, snow
