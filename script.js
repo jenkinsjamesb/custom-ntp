d4d30d91e8400a2abaaa8dc000525340549e8ff9
Brad J <jenkins.j.brad@gmail.com>
2022-05-21 21:33:41 +0400

    Add popup with show options toggle.

--- script.js
+++ script.js
@@ -5,11 +5,66 @@ document.addEventListener("DOMContentLoaded", function () {
         elem.style.width = (elem.value.length + 1) + 'ch';
     }
 
+    chrome.storage.sync.get({ optionsEnabled: false }, function(result) {
+        optionsEnabled = result.optionsEnabled;
+
+        if (optionsEnabled) {
+            document.getElementById("options").style.display = "flex";
+        }
+    });
+
+    chrome.storage.sync.get({ customImage: "" }, function(result) {
+        customImage = result.customImage;
+                
+    });
+
     // import background
-    weatherEnabled = false;
-    if (weatherEnabled) {
-        var script = document.createElement("script");
-        script.src = "resources/weather.js"
-        document.head.appendChild(script)
-    };
+    chrome.storage.sync.get({ weatherEnabled: false }, function(result) {
+        weatherEnabled = result.weatherEnabled;
+
+        if (weatherEnabled) {
+            var script = document.createElement("script");
+            script.src = "resources/weather.js"
+            document.head.appendChild(script)
+        }
+    });
+});
+
+function save_options() {
+var bool = document.getElementById("weather-input").checked;
+var str = document.getElementById("image-input");
+chrome.storage.sync.set({
+    weatherEnabled: bool,
+    customImage: str
+}, function() {
+    var status = document.getElementById('status');
+    status.textContent = 'Saved.';
+    setTimeout(function() {
+    status.textContent = '';
+    }, 1500);
+});
+}
+
+// restores select box and checkbox state using the preferences stored in chrome.storage.
+function restore_options() {
+chrome.storage.sync.get({
+    weatherEnabled: false,
+    customImage: ""
+}, function(items) {
+    document.getElementById("weather-input").checked = items.weatherEnabled;
+    try {
+        document.getElementById("image").src = URL.createObjectURL(items.customImage.files);
+    } catch {
+
+    }
+});
+}
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("weather-input").addEventListener("click", save_options);
+document.getElementById("image-input").addEventListener("input", save_options);
+
+document.getElementById("reset").addEventListener("click", () => {
+    document.getElementById("weather-input").checked = false;
+    document.getElementById("image-input").value = "";
+    save_options();
 });
